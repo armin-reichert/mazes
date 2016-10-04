741287b98f75c9298d36f69a78a0225f80274323
Armin Reichert <armin.reichert@web.de>
2016-10-04 16:30:10 +0200

    clarified

--- EasyMaze/src/de/amr/easy/maze/algorithms/KruskalMST.java
+++ EasyMaze/src/de/amr/easy/maze/algorithms/KruskalMST.java
@@ -2,10 +2,11 @@ package de.amr.easy.maze.algorithms;
 
 import static de.amr.easy.graph.api.TraversalState.COMPLETED;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import de.amr.easy.graph.api.TraversalState;
 import de.amr.easy.graph.impl.DefaultEdge;
@@ -16,15 +17,12 @@ import de.amr.easy.maze.datastructures.Partition.EquivClass;
 /**
  * Maze generator derived from Kruskal's minimum spanning tree algorithm.
  * 
- * <p>
- * Instead of always selecting the edge with minimal weight, the maze algorithm shuffles the edge
- * set of a full grid and takes the edges from this list.
- * 
  * @author Armin Reichert
  */
 public class KruskalMST implements Consumer<Integer> {
 
 	private final ObservableDataGrid2D<Integer, DefaultEdge<Integer>, TraversalState> grid;
+
 	// Note: The partition has not to be initialized because the find-operation
 	// creates new equivalence-classes on demand.
 	private final Partition<Integer> forest = new Partition<>();
@@ -47,15 +45,13 @@ public class KruskalMST implements Consumer<Integer> {
 		});
 	}
 
-	private Iterable<DefaultEdge<Integer>> fullGridEdgesInRandomOrder() {
-		int fullGridEdgeCount = 2 * grid.numCols() * grid.numRows() - grid.numRows() - grid.numCols();
-		List<DefaultEdge<Integer>> edges = new ArrayList<>(fullGridEdgeCount);
+	private Stream<DefaultEdge<Integer>> fullGridEdgesInRandomOrder() {
 		grid.setEventsEnabled(false);
 		grid.fillAllEdges();
-		grid.edgeStream().forEach(edges::add);
+		List<DefaultEdge<Integer>> edges = grid.edgeStream().collect(Collectors.toList());
+		Collections.shuffle(edges); // takes linear time
 		grid.removeEdges();
 		grid.setEventsEnabled(true);
-		Collections.shuffle(edges); // takes linear time
-		return edges;
+		return edges.stream();
 	}
 }
