041a5469d3628c7d42b11832645e7d22f2a55cdf
Armin Reichert <armin.reichert@web.de>
2017-06-26 14:00:05 +0200

    doc

--- EasyGrid/src/de/amr/easy/grid/impl/Top4.java
+++ EasyGrid/src/de/amr/easy/grid/impl/Top4.java
@@ -12,18 +12,21 @@ import de.amr.easy.grid.api.Topology;
 public class Top4 implements Topology {
 
 	public static final Top4 Top4 = new Top4();
-	
+
 	private static final int DX[] = { 0, 1, 0, -1 };
 	private static final int DY[] = { -1, 0, 1, 0 };
-	private static final String NAMES[] = { "N", "E", "S", "W" };
+	private static final String NAMES[] = { "North", "East", "South", "West" };
 
+	/** North */
 	public static final int N = 0;
+	/** East */
 	public static final int E = 1;
+	/** South */
 	public static final int S = 2;
+	/** West */
 	public static final int W = 3;
-	
+
 	private Top4() {
-		
 	}
 
 	@Override
