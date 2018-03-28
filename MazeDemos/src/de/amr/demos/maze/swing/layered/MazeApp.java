958a65707227d61810a8a5693318d49839c92e20
Armin Reichert <armin.reichert@web.de>
2018-03-28 15:12:46 +0200

    refactor

--- MazeDemos/src/de/amr/demos/maze/swing/layered/MazeApp.java
+++ MazeDemos/src/de/amr/demos/maze/swing/layered/MazeApp.java
@@ -1,6 +1,7 @@
 package de.amr.demos.maze.swing.layered;
 
 import static de.amr.easy.grid.api.GridPosition.BOTTOM_RIGHT;
+import static de.amr.easy.grid.api.GridPosition.CENTER;
 import static de.amr.easy.grid.api.GridPosition.TOP_LEFT;
 
 import java.awt.EventQueue;
@@ -9,10 +10,12 @@ import javax.swing.Timer;
 import javax.swing.UIManager;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 
+import de.amr.demos.grid.swing.ui.GridCanvas;
+import de.amr.demos.grid.swing.ui.GridWindow;
 import de.amr.easy.graph.alg.traversal.BreadthFirstTraversal;
 import de.amr.easy.graph.api.TraversalState;
 import de.amr.easy.grid.api.Grid2D;
-import de.amr.easy.maze.alg.PrimMST;
+import de.amr.easy.maze.alg.wilson.WilsonUSTRandomCell;
 
 public class MazeApp {
 
@@ -28,21 +31,23 @@ public class MazeApp {
 	public MazeApp() {
 		GridCanvas canvas = new GridCanvas(1000, 800, 20);
 		GridWindow window = new GridWindow(canvas);
-		window.pack();
-		window.setVisible(true);
 
-		Timer timer = new Timer(3000, event -> {
-			Grid2D<TraversalState, Integer> grid = canvas.getGrid();
-			canvas.clear();
+		Timer timer = new Timer(1000, evt -> {
+			final Grid2D<TraversalState, Integer> grid = canvas.getGrid();
 			grid.clearContent();
 			grid.removeEdges();
-			new PrimMST(grid).run(0);
+			new WilsonUSTRandomCell(grid).run(grid.cell(CENTER));
 			canvas.setBfs(new BreadthFirstTraversal<>(grid, grid.cell(TOP_LEFT)));
 			canvas.getBfs().run();
 			canvas.setPath(canvas.getBfs().findPath(grid.cell(BOTTOM_RIGHT)));
+			canvas.clear();
 			canvas.repaint();
 		});
 		timer.setInitialDelay(0);
+
+		window.pack();
+		window.setVisible(true);
+
 		timer.start();
 	}
 }
