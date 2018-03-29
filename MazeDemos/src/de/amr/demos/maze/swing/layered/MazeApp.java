49cafe0d3281dddb22e8118ac694d4e1ad0b5a5d
Armin Reichert <armin.reichert@web.de>
2018-03-29 13:10:42 +0200

    improved

--- MazeDemos/src/de/amr/demos/maze/swing/layered/MazeApp.java
+++ MazeDemos/src/de/amr/demos/maze/swing/layered/MazeApp.java
@@ -15,13 +15,12 @@ import javax.swing.ButtonGroup;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JRadioButtonMenuItem;
-import javax.swing.Timer;
+import javax.swing.KeyStroke;
 import javax.swing.UIManager;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 
 import de.amr.demos.grid.swing.ui.GridCanvas;
 import de.amr.demos.grid.swing.ui.GridWindow;
-import de.amr.easy.graph.alg.traversal.BreadthFirstTraversal;
 import de.amr.easy.graph.api.TraversalState;
 import de.amr.easy.grid.api.Grid2D;
 import de.amr.easy.grid.api.GridPosition;
@@ -38,66 +37,76 @@ public class MazeApp {
 		EventQueue.invokeLater(MazeApp::new);
 	}
 
+	private final GridCanvas canvas;
 	private GridPosition pathStart;
 	private GridPosition pathTarget;
 
 	public MazeApp() {
+		canvas = new GridCanvas(1200, 900, 30);
 		pathStart = TOP_LEFT;
 		pathTarget = BOTTOM_RIGHT;
-		GridCanvas canvas = new GridCanvas(1000, 800, 20);
-		Timer timer = new Timer(2500, evt -> {
+
+		GridWindow window = new GridWindow(canvas);
+		addMenuPath(window);
+		window.pack();
+		window.setVisible(true);
+
+		canvas.getInputMap().put(KeyStroke.getKeyStroke("SPACE"), "nextMaze");
+		canvas.getActionMap().put("nextMaze", nextMazeAction);
+	}
+
+	private Action nextMazeAction = new AbstractAction() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
 			final Grid2D<TraversalState, Integer> grid = canvas.getGrid();
 			grid.clearContent();
 			grid.removeEdges();
 			new WilsonUSTRandomCell(grid).run(grid.cell(CENTER));
-			BreadthFirstTraversal<Integer, ?> bfs = new BreadthFirstTraversal<>(grid, grid.cell(pathStart));
-			bfs.run();
-			canvas.setBfs(bfs);
-			canvas.setPath(bfs.findPath(grid.cell(pathTarget)));
 			canvas.clear();
+			canvas.runPathFinder(grid.cell(pathStart), grid.cell(pathTarget));
 			canvas.repaint();
-		});
-		timer.setInitialDelay(0);
+		}
+	};
 
-		GridWindow window = new GridWindow(canvas);
-		addPathMenu(window);
-		window.pack();
-		window.setVisible(true);
-
-		timer.start();
-	}
-
-	private void addPathMenu(GridWindow window) {
+	private void addMenuPath(GridWindow window) {
 		JMenu menuPathStart = new JMenu("Path Start");
 		window.getMenuOptions().add(menuPathStart);
-		addGridPositionMenu(menuPathStart, pathStart, new AbstractAction() {
+		addMenuGridPositions(menuPathStart, pathStart, new AbstractAction() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				JMenuItem item = (JMenuItem) e.getSource();
 				pathStart = (GridPosition) item.getClientProperty("position");
+				canvas.clear();
+				final Grid2D<TraversalState, Integer> grid = canvas.getGrid();
+				canvas.runPathFinder(grid.cell(pathStart), grid.cell(pathTarget));
+				canvas.repaint();
 			}
 		});
 
 		JMenu menuPathTarget = new JMenu("Path Target");
 		window.getMenuOptions().add(menuPathTarget);
-		addGridPositionMenu(menuPathTarget, pathTarget, new AbstractAction() {
+		addMenuGridPositions(menuPathTarget, pathTarget, new AbstractAction() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				JMenuItem item = (JMenuItem) e.getSource();
 				pathTarget = (GridPosition) item.getClientProperty("position");
+				canvas.clear();
+				final Grid2D<TraversalState, Integer> grid = canvas.getGrid();
+				canvas.runPathFinder(grid.cell(pathStart), grid.cell(pathTarget));
+				canvas.repaint();
 			}
 		});
 	}
 
-	private void addGridPositionMenu(JMenu menu, GridPosition selection, Action action) {
+	private void addMenuGridPositions(JMenu menu, GridPosition selection, Action action) {
 		final String[] texts = { "Top Left", "Top Right", "Bottom Left", "Bottom Right", "Center" };
 		final GridPosition[] positions = { TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT, CENTER };
-		ButtonGroup bg = new ButtonGroup();
+		final ButtonGroup bg = new ButtonGroup();
 		for (int i = 0; i < texts.length; ++i) {
-			JRadioButtonMenuItem mi = new JRadioButtonMenuItem();
-			mi.setAction(action);
+			JRadioButtonMenuItem mi = new JRadioButtonMenuItem(action);
 			mi.setText(texts[i]);
 			mi.putClientProperty("position", positions[i]);
 			mi.setSelected(positions[i] == selection);
