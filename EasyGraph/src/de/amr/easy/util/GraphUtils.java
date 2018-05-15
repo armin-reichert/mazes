965c4319d9605d5ab17039e57c19326ab7cdb0b4
Armin Reichert <armin.reichert@web.de>
2018-05-15 12:46:52 +0200

    doc

--- EasyGraph/src/de/amr/easy/util/GraphUtils.java
+++ EasyGraph/src/de/amr/easy/util/GraphUtils.java
@@ -4,8 +4,22 @@ import de.amr.easy.data.Partition;
 import de.amr.easy.graph.api.Edge;
 import de.amr.easy.graph.api.Graph;
 
+/**
+ * Some useful graph methods.
+ * 
+ * @author Armin Reichert
+ */
 public class GraphUtils {
 
+	/**
+	 * Checks whether a graph contains a cycle.
+	 * 
+	 * @param <E>
+	 *          the edge type of the graph
+	 * @param g
+	 *          an undirected graph
+	 * @return {@code true} if the graph contains a cycle
+	 */
 	public static <E extends Edge> boolean containsCycle(Graph<E> g) {
 		Partition<Integer> p = new Partition<>(g.vertexStream().boxed());
 		Iterable<E> edges = g.edgeStream()::iterator;
