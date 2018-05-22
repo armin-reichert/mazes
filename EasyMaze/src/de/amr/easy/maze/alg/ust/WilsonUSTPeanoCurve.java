3a06fce69dc6abbb08120841a1a96d08798462b2
Armin Reichert <armin.reichert@web.de>
2018-05-22 10:33:42 +0200

    improved

--- EasyMaze/src/de/amr/easy/maze/alg/ust/WilsonUSTPeanoCurve.java
+++ EasyMaze/src/de/amr/easy/maze/alg/ust/WilsonUSTPeanoCurve.java
@@ -1,10 +1,11 @@
 package de.amr.easy.maze.alg.ust;
 
 import static de.amr.easy.grid.api.GridPosition.BOTTOM_LEFT;
+import static de.amr.easy.util.GridUtils.log;
 import static de.amr.easy.util.GridUtils.nextPow;
 import static java.lang.Math.max;
+import static java.util.Arrays.stream;
 
-import java.util.Arrays;
 import java.util.stream.IntStream;
 
 import de.amr.easy.graph.api.TraversalState;
@@ -12,39 +13,41 @@ import de.amr.easy.grid.api.Grid2D;
 import de.amr.easy.grid.curves.PeanoCurve;
 import de.amr.easy.grid.impl.BareGrid;
 import de.amr.easy.grid.impl.Top4;
-import de.amr.easy.util.GridUtils;
 
 /**
- * Wilson's algorithm where the vertices are selected from a Peano-curve.
+ * Wilson's algorithm where the random walks start in the order defined by a Peano curve.
  * 
  * @author Armin Reichert
  */
 public class WilsonUSTPeanoCurve extends WilsonUST {
 
-	private final int[] path;
+	private final int[] walkStartCells;
+	private final BareGrid<?> square;
+	private final PeanoCurve peano;
 	private int i;
 
 	public WilsonUSTPeanoCurve(Grid2D<TraversalState, Integer> grid) {
 		super(grid);
-		path = new int[grid.numCells()];
+		walkStartCells = new int[grid.numCells()];
+		int n = nextPow(3, max(grid.numCols(), grid.numRows()));
+		square = new BareGrid<>(n, n, Top4.get());
+		peano = new PeanoCurve(log(3, n));
+		int current = square.cell(BOTTOM_LEFT);
+		addWalkStartCell(square.col(current), square.row(current));
+		for (int dir : peano) {
+			current = square.neighbor(current, dir).getAsInt();
+			addWalkStartCell(square.col(current), square.row(current));
+		}
 	}
 
 	@Override
 	protected IntStream cellStream() {
-		int n = nextPow(3, max(grid.numCols(), grid.numRows()));
-		BareGrid<?> square = new BareGrid<>(n, n, Top4.get());
-		Integer cell = square.cell(BOTTOM_LEFT);
-		addCellToPath(square.col(cell), square.row(cell));
-		for (int dir : new PeanoCurve(GridUtils.log(3, n))) {
-			cell = square.neighbor(cell, dir).getAsInt();
-			addCellToPath(square.col(cell), square.row(cell));
-		}
-		return Arrays.stream(path);
+		return stream(walkStartCells);
 	}
 
-	private void addCellToPath(int col, int row) {
+	private void addWalkStartCell(int col, int row) {
 		if (grid.isValidCol(col) && grid.isValidRow(row)) {
-			path[i++] = grid.cell(col, row);
+			walkStartCells[i++] = grid.cell(col, row);
 		}
 	}
 }
