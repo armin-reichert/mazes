d0eabbda91ee228b5a870a7d5dfee8b5c13a7fe1
Armin Reichert <armin.reichert@web.de>
2018-05-22 12:22:50 +0200

    tests

--- MazesUnitTests/src/de/amr/easy/maze/tests/LargeMazesTests.java
+++ MazesUnitTests/src/de/amr/easy/maze/tests/LargeMazesTests.java
@@ -8,13 +8,19 @@ import de.amr.easy.graph.api.TraversalState;
 import de.amr.easy.grid.api.Grid2D;
 import de.amr.easy.grid.impl.Grid;
 import de.amr.easy.grid.impl.Top4;
+import de.amr.easy.maze.alg.RecursiveDivision;
 import de.amr.easy.maze.alg.mst.KruskalMST;
 
 public class LargeMazesTests {
 
 	private void test_Kruskal(int cols, int rows) {
 		Grid2D<TraversalState, Integer> grid = new Grid<>(cols, rows, Top4.get(), UNVISITED, false);
-		new KruskalMST(grid).run(0);
+		new KruskalMST(grid).run(-1);
+	}
+	
+	private void test_RecursiveDivision(int cols, int rows) {
+		Grid2D<TraversalState, Integer> grid = new Grid<>(cols, rows, Top4.get(), UNVISITED, false);
+		new RecursiveDivision(grid).run(-1);
 	}
 
 	@Test
@@ -23,17 +29,27 @@ public class LargeMazesTests {
 	}
 
 	@Test
-	public void test_Kruskal_200_000() {
-		test_Kruskal(200, 1000);
+	public void test_Kruskal_500_000() {
+		test_Kruskal(500, 1000);
 	}
 
 	@Test
-	public void test_Kruskal_400_000() {
-		test_Kruskal(400, 1000);
+	public void test_Kruskal_1_000_000() {
+		test_Kruskal(1000, 1000);
 	}
 
 	@Test
-	public void test_Kruskal_800_000() {
-		test_Kruskal(800, 1000);
+	public void test_RecursiveDivision_100_000() {
+		test_RecursiveDivision(100, 1000);
+	}
+
+	@Test
+	public void test_RecursiveDivision_500_000() {
+		test_RecursiveDivision(500, 1000);
+	}
+
+	@Test
+	public void test_RecursiveDivision_1_000_000() {
+		test_RecursiveDivision(1000, 1000);
 	}
 }
