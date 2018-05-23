8bfe69ad86811ecb473f1063f78614a047ef4285
Armin Reichert <armin.reichert@web.de>
2018-05-23 11:15:18 +0200

    fix names

--- EasyMaze/src/de/amr/easy/maze/alg/Eller.java
+++ EasyMaze/src/de/amr/easy/maze/alg/Eller.java
@@ -23,7 +23,7 @@ import de.amr.easy.grid.api.Grid2D;
  */
 public class Eller extends MazeAlgorithm {
 
-	private final Partition<Integer> partition = new Partition<>();
+	private final Partition<Integer> parts = new Partition<>();
 
 	public Eller(Grid2D<TraversalState, Integer> grid) {
 		super(grid);
@@ -40,43 +40,44 @@ public class Eller extends MazeAlgorithm {
 
 	private void connectCells(int u, int v) {
 		addEdge(u, v);
-		partition.union(u, v);
+		parts.union(u, v);
 	}
 
 	private void connectCellsInsideRow(int row, boolean all) {
-		range(0, grid.numCols() - 1).filter(cell -> all || rnd.nextBoolean()).forEach(col -> {
+		range(0, grid.numCols() - 1).filter(col -> all || rnd.nextBoolean()).forEach(col -> {
 			int left = grid.cell(col, row), right = grid.cell(col + 1, row);
-			if (partition.find(left) != partition.find(right)) {
+			if (parts.find(left) != parts.find(right)) {
 				connectCells(left, right);
 			}
 		});
 	}
 
 	private void connectCellsWithNextRow(int row) {
-		Set<Partition<Integer>.Set> connectedSets = new HashSet<>();
-		range(0, grid.numCols()).filter(cell -> rnd.nextBoolean()).forEach(col -> {
-			int above = grid.cell(col, row), below = grid.cell(col, row + 1);
-			connectCells(above, below);
-			connectedSets.add(partition.find(above));
+		// connect randomly selected cells of this row with next row
+		Set<Partition<Integer>.Set> connectedParts = new HashSet<>();
+		range(0, grid.numCols()).filter(col -> rnd.nextBoolean()).forEach(col -> {
+			int top = grid.cell(col, row), bottom = grid.cell(col, row + 1);
+			connectCells(top, bottom);
+			connectedParts.add(parts.find(top));
 		});
-		// collect cells of still unconnected components
-		List<Integer> unconnectedSets = new ArrayList<>();
+		// collect cells of still unconnected parts in this row
+		List<Integer> unconnectedCells = new ArrayList<>();
 		range(0, grid.numCols()).forEach(col -> {
 			int cell = grid.cell(col, row);
-			Partition<Integer>.Set set = partition.find(cell);
-			if (!connectedSets.contains(set)) {
-				unconnectedSets.add(cell);
+			Partition<Integer>.Set part = parts.find(cell);
+			if (!connectedParts.contains(part)) {
+				unconnectedCells.add(cell);
 			}
 		});
-		// shuffle cells to avoid biased maze
-		Collections.shuffle(unconnectedSets);
+		// shuffle unconnected cells to avoid biased maze
+		Collections.shuffle(unconnectedCells);
 		// connect cells and mark component as connected
-		unconnectedSets.forEach(above -> {
-			Partition<Integer>.Set set = partition.find(above);
-			if (!connectedSets.contains(set)) {
-				int below = grid.cell(grid.col(above), row + 1);
-				connectCells(above, below);
-				connectedSets.add(set);
+		unconnectedCells.forEach(top -> {
+			Partition<Integer>.Set part = parts.find(top);
+			if (!connectedParts.contains(part)) {
+				int bottom = grid.cell(grid.col(top), row + 1);
+				connectCells(top, bottom);
+				connectedParts.add(part);
 			}
 		});
 	}
