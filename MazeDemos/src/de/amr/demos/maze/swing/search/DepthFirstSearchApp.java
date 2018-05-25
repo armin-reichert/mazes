e7e5ad6b025485179dd16a906f310056f688361e
Armin Reichert <armin.reichert@web.de>
2018-05-25 15:59:17 +0200

    improved

--- MazeDemos/src/de/amr/demos/maze/swing/search/DepthFirstSearchApp.java
+++ MazeDemos/src/de/amr/demos/maze/swing/search/DepthFirstSearchApp.java
@@ -1,5 +1,6 @@
 package de.amr.demos.maze.swing.search;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.EventQueue;
@@ -35,21 +36,19 @@ public class DepthFirstSearchApp {
 	}
 
 	private Grid2D<TraversalState, Integer> grid;
-
-	private DepthFirstTraversal dfs;
-	private int source;
-	private int target;
 	private int[] solution;
-
 	private DrawingArea canvas;
 	private ConfigurableGridRenderer renderer;
 
 	private class DrawingArea extends JComponent {
 
 		@Override
-		protected void paintComponent(Graphics g_) {
-			Graphics2D g = (Graphics2D) g_;
+		protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
+			drawGrid((Graphics2D) g);
+		}
+
+		private void drawGrid(Graphics2D g) {
 			g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 			g.setColor(renderer.getGridBgColor());
 			g.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
@@ -62,13 +61,12 @@ public class DepthFirstSearchApp {
 		private void drawSolution(Graphics2D g) {
 			int prev = -1;
 			int cs = renderer.getCellSize();
-			int diam = renderer.getPassageWidth() / 4;
 			for (int cell : solution) {
 				int x = cs * grid.col(cell) + cs / 2, y = cs * grid.row(cell) + cs / 2;
-				g.setColor(Color.BLUE);
-				g.fillOval(x - diam / 2, y - diam / 2, diam, diam);
 				if (prev != -1) {
 					int px = cs * grid.col(prev) + cs / 2, py = cs * grid.row(prev) + cs / 2;
+					g.setColor(Color.BLUE);
+					g.setStroke(new BasicStroke(3));
 					g.drawLine(px, py, x, y);
 				}
 				prev = cell;
@@ -79,28 +77,24 @@ public class DepthFirstSearchApp {
 	public DepthFirstSearchApp() {
 		newMaze(8);
 
-		// Canvas
 		canvas = new DrawingArea();
-		canvas.setPreferredSize(new Dimension(1024, 1024));
+		canvas.setPreferredSize(new Dimension(800, 800));
 
-		// Renderer
 		renderer = new ConfigurableGridRenderer();
 		renderer.fnCellBgColor = cell -> Color.WHITE;
-		renderer.fnCellSize = () -> canvas.getHeight() / grid.numRows();
+		renderer.fnCellSize = () -> Math.min(canvas.getHeight(), canvas.getWidth()) / grid.numRows();
 		renderer.fnGridBgColor = () -> Color.BLACK;
-		renderer.fnPassageColor = (cell, dir) -> Color.WHITE;
-		renderer.fnPassageWidth = () -> renderer.getCellSize() / 4;
+		renderer.fnPassageColor = (cell, dir) -> renderer.getCellBgColor(cell);
+		renderer.fnPassageWidth = () -> renderer.getCellSize() * 90 / 100;
 		renderer.fnText = cell -> String.format("%d", cell);
-		renderer.fnTextColor = () -> Color.RED;
-		renderer.fnTextFont = () -> new Font("Arial Bold", Font.PLAIN, renderer.getPassageWidth() - 3);
+		renderer.fnTextColor = () -> Color.LIGHT_GRAY;
+		renderer.fnTextFont = () -> new Font(Font.SANS_SERIF, Font.PLAIN, renderer.getCellSize() / 3);
 
-		// Keyboard Actions
 		addKeyboardAction('s', this::dfs);
 		addKeyboardAction(' ', this::newMaze);
 		addKeyboardAction('+', this::largerMaze);
 		addKeyboardAction('-', this::smallerMaze);
 
-		// Window
 		JFrame window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setTitle("DFS Test Application");
@@ -124,7 +118,7 @@ public class DepthFirstSearchApp {
 	}
 
 	private void largerMaze() {
-		if (grid.numRows() < canvas.getHeight() / 2) {
+		if (grid.numRows() < canvas.getHeight() / 4) {
 			newMaze(2 * grid.numRows());
 		}
 	}
@@ -137,8 +131,6 @@ public class DepthFirstSearchApp {
 
 	private void newMaze(int gridSize) {
 		grid = new Grid<>(gridSize, gridSize, Top4.get(), TraversalState.UNVISITED, false);
-		source = grid.cell(GridPosition.TOP_LEFT);
-		target = grid.cell(GridPosition.BOTTOM_RIGHT);
 		solution = null;
 		new KruskalMST(grid).run(0);
 	}
@@ -148,8 +140,9 @@ public class DepthFirstSearchApp {
 	}
 
 	private void dfs() {
-		dfs = new DepthFirstTraversal(grid, source, target);
+		DepthFirstTraversal dfs = new DepthFirstTraversal(grid, grid.cell(GridPosition.TOP_LEFT),
+				grid.cell(GridPosition.BOTTOM_RIGHT));
 		dfs.traverseGraph();
-		solution = dfs.findPath(target).toArray();
+		solution = dfs.findPath(grid.cell(GridPosition.BOTTOM_RIGHT)).toArray();
 	}
 }
