892f95d6aae4486a8b411e25e48ab98bb746a05d
Armin Reichert <armin.reichert@web.de>
2018-05-27 16:27:39 +0200

    cleanup

--- SwingMazeDemo/src/de/amr/demos/maze/swingapp/view/PathFinderMenu.java
+++ SwingMazeDemo/src/de/amr/demos/maze/swingapp/view/PathFinderMenu.java
@@ -1,8 +1,7 @@
 package de.amr.demos.maze.swingapp.view;
 
 import static de.amr.demos.maze.swingapp.model.MazeDemoModel.PATHFINDER_ALGORITHMS;
-
-import java.awt.event.ActionEvent;
+import static java.util.Arrays.stream;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JMenu;
@@ -13,29 +12,20 @@ import de.amr.demos.maze.swingapp.model.AlgorithmInfo;
 public class PathFinderMenu extends JMenu {
 
 	private AlgorithmInfo<?> selectedPathFinder;
+	private ButtonGroup group = new ButtonGroup();
 
 	public PathFinderMenu() {
 		super("Pathfinders");
-		ButtonGroup group = new ButtonGroup();
-		{
-			JRadioButtonMenuItem item = new JRadioButtonMenuItem();
-			item.setText("No Pathfinder");
-			item.addActionListener((ActionEvent e) -> {
-				setSelectedPathFinder(null);
-			});
-			add(item);
-			group.add(item);
-		}
-		for (AlgorithmInfo<?> pathFinder : PATHFINDER_ALGORITHMS) {
-			JRadioButtonMenuItem item = new JRadioButtonMenuItem();
-			item.setText(pathFinder.getDescription());
-			item.addActionListener((ActionEvent e) -> {
-				setSelectedPathFinder(pathFinder);
-			});
-			add(item);
-			group.add(item);
-		}
-		getItem(0).setSelected(true);
+		addItem("No Pathfinder", null).setSelected(true);
+		stream(PATHFINDER_ALGORITHMS).forEach(alg -> addItem(alg.getDescription(), alg));
+	}
+
+	private JRadioButtonMenuItem addItem(String text, AlgorithmInfo<?> pathFinder) {
+		JRadioButtonMenuItem item = new JRadioButtonMenuItem(text);
+		item.addActionListener(e -> setSelectedPathFinder(pathFinder));
+		add(item);
+		group.add(item);
+		return item;
 	}
 
 	public void setSelectedPathFinder(AlgorithmInfo<?> alg) {
