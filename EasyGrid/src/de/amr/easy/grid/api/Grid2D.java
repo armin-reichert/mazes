9510b608323295c28c6bd43a160107dad24fefa9
Armin Reichert <armin.reichert@web.de>
2018-06-02 10:42:11 +0200

    simplified

--- EasyGrid/src/de/amr/easy/grid/api/Grid2D.java
+++ EasyGrid/src/de/amr/easy/grid/api/Grid2D.java
@@ -1,19 +1,16 @@
 package de.amr.easy.grid.api;
 
-import de.amr.easy.graph.api.DataGraph;
 import de.amr.easy.graph.api.VertexContent;
-import de.amr.easy.graph.api.WeightedEdge;
 
 /**
- * A 2-D grid with the ability to store data inside its "cells".
+ * A 2D-grid graph with the ability to store content inside its "cells".
  * 
  * @author Armin Reichert
  *
  * @param <C>
- *          cell content type
+ *          cell/vertex content type
  * @param <W>
- *          passage weight type
+ *          passage/edge weight type
  */
-public interface Grid2D<C, W extends Comparable<W>>
-		extends BareGrid2D<W>, VertexContent<C>, DataGraph<WeightedEdge<W>, C> {
+public interface Grid2D<C, W extends Comparable<W>> extends BareGrid2D<W>, VertexContent<C> {
 }
