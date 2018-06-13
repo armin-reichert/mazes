47a03738e4dfb79d60b616a88f5304d9646855d9
Armin Reichert <armin.reichert@web.de>
2018-06-13 12:08:37 +0200

    improved

--- SwingMazeDemo/src/de/amr/demos/maze/swingapp/view/menu/GeneratorMenu.java
+++ SwingMazeDemo/src/de/amr/demos/maze/swingapp/view/menu/GeneratorMenu.java
@@ -5,12 +5,11 @@ import static de.amr.demos.maze.swingapp.model.MazeGenerationAlgorithmTag.MST;
 import static de.amr.demos.maze.swingapp.model.MazeGenerationAlgorithmTag.Traversal;
 import static de.amr.demos.maze.swingapp.model.MazeGenerationAlgorithmTag.UST;
 
-import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
-import javax.swing.AbstractAction;
-import javax.swing.Action;
+import javax.swing.AbstractButton;
 import javax.swing.JMenu;
 import javax.swing.JRadioButtonMenuItem;
 
@@ -24,17 +23,10 @@ import de.amr.demos.maze.swingapp.model.AlgorithmInfo;
  */
 public class GeneratorMenu extends AlgorithmMenu {
 
-	private final Action onSelectionAction;
+	private final ActionListener onSelection;
 
 	public GeneratorMenu(MazeDemoApp app) {
-		onSelectionAction = new AbstractAction() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				JRadioButtonMenuItem item = (JRadioButtonMenuItem) e.getSource();
-				app.setGeneratorName(item.getText());
-			}
-		};
+		onSelection = e -> app.setSolverName(((AbstractButton) e.getSource()).getText());
 		setText("Generators");
 		addMenu("Graph Traversal", alg -> alg.isTagged(Traversal));
 		addMenu("Minimum Spanning Tree", alg -> alg.isTagged(MST));
@@ -45,7 +37,8 @@ public class GeneratorMenu extends AlgorithmMenu {
 	private void addMenu(String title, Predicate<AlgorithmInfo> filter) {
 		JMenu menu = new JMenu(title);
 		Stream.of(GENERATOR_ALGORITHMS).filter(filter).forEach(alg -> {
-			JRadioButtonMenuItem item = new JRadioButtonMenuItem(onSelectionAction);
+			JRadioButtonMenuItem item = new JRadioButtonMenuItem();
+			item.addActionListener(onSelection);
 			item.setText(alg.getDescription());
 			item.putClientProperty("algorithm", alg);
 			btnGroup.add(item);
