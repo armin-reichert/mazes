c210d9303d4d2a17a966e351d45b464275f41cbd
Armin Reichert <armin.reichert@web.de>
2018-06-13 12:02:07 +0200

    improved

--- SwingMazeDemo/src/de/amr/demos/maze/swingapp/view/menu/SolverMenu.java
+++ SwingMazeDemo/src/de/amr/demos/maze/swingapp/view/menu/SolverMenu.java
@@ -2,11 +2,10 @@ package de.amr.demos.maze.swingapp.view.menu;
 
 import static de.amr.demos.maze.swingapp.model.MazeDemoModel.PATHFINDER_ALGORITHMS;
 
-import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.stream.Stream;
 
-import javax.swing.AbstractAction;
-import javax.swing.Action;
+import javax.swing.AbstractButton;
 import javax.swing.JRadioButtonMenuItem;
 
 import de.amr.demos.maze.swingapp.MazeDemoApp;
@@ -19,20 +18,13 @@ import de.amr.demos.maze.swingapp.model.AlgorithmInfo;
  */
 public class SolverMenu extends AlgorithmMenu {
 
-	private final Action onSelectionAction;
+	private final ActionListener onSelection;
 
 	public SolverMenu(MazeDemoApp app) {
-		onSelectionAction = new AbstractAction() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				JRadioButtonMenuItem item = (JRadioButtonMenuItem) e.getSource();
-				app.setSolverName(item.getText());
-			}
-		};
+		onSelection = e -> app.setSolverName(((AbstractButton) e.getSource()).getText());
 		setText("Solvers");
-		add(app.actionFloodFill);
 		add(app.actionClearCanvas);
+		add(app.actionFloodFill);
 		addSeparator();
 		Stream.of(PATHFINDER_ALGORITHMS).forEach(this::addMenuItem);
 	}
@@ -41,7 +33,8 @@ public class SolverMenu extends AlgorithmMenu {
 		if (alg == null) {
 			addSeparator();
 		} else {
-			JRadioButtonMenuItem item = new JRadioButtonMenuItem(onSelectionAction);
+			JRadioButtonMenuItem item = new JRadioButtonMenuItem();
+			item.addActionListener(onSelection);
 			item.setText(alg.getDescription());
 			item.putClientProperty("algorithm", alg);
 			add(item);
