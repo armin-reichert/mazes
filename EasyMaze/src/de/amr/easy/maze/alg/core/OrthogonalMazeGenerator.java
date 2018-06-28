938975dacc96db051131d8afc1590cb66f76ae34
Armin Reichert <armin.reichert@web.de>
2018-06-28 14:07:07 +0200

    doc

--- EasyMaze/src/de/amr/easy/maze/alg/core/OrthogonalMazeGenerator.java
+++ EasyMaze/src/de/amr/easy/maze/alg/core/OrthogonalMazeGenerator.java
@@ -1,13 +1,25 @@
 package de.amr.easy.maze.alg.core;
 
 /**
- * Maze generator base class.
+ * Maze generator interface.
  * 
  * @author Armin Reichert
  */
 public interface OrthogonalMazeGenerator {
-	
+
+	/**
+	 * @return the grid this generator operates upon
+	 */
 	OrthogonalGrid getGrid();
 
+	/**
+	 * Creates a maze starting at the grid cell {@code (x, y)}.
+	 * 
+	 * @param x
+	 *          x-coordinate (column) of start cell
+	 * @param y
+	 *          y-coordinate (row) of start cell
+	 * @return maze (spanning tree)
+	 */
 	OrthogonalGrid createMaze(int x, int y);
 }
