4564b41587f1e36bd28be8c284f394674aa1f216
Armin Reichert <armin.reichert@web.de>
2019-01-10 09:00:44 +0100

    unit test

---
+++ SimpleMaze/src/test/java/de/amr/mazes/simple/test/LargeMazeTest.java
@@ -0,0 +1,40 @@
+package de.amr.mazes.simple.test;
+
+import static de.amr.mazes.simple.MazeGeneration.createMazeByDFS;
+
+import java.util.function.Consumer;
+
+import org.junit.Test;
+
+import de.amr.mazes.simple.Grid;
+
+public class LargeMazeTest {
+
+	static Grid test(String name, Consumer<Grid> generator, int rows, int cols) {
+		Grid grid = new Grid(rows, cols);
+		long start = System.currentTimeMillis();
+		generator.accept(grid);
+		long time = System.currentTimeMillis() - start;
+		System.out.println(String.format(name + ": %,d vertices, time %d ms", grid.numVertices(), time));
+		if (grid.numEdges() != grid.numVertices() - 1) {
+			throw new IllegalStateException(
+					String.format("Wrong #edges: %d (expected %d)", grid.numEdges(), grid.numVertices() - 1));
+		}
+		return grid;
+	}
+
+//	@Test
+	public void test_DFS_100_000() {
+		test("Non-Recursive DFS", grid -> createMazeByDFS(grid, 0), 100, 1000);
+	}
+
+	@Test
+	public void test_DFS_1_000_000() {
+		test("Non-Recursive DFS", grid -> createMazeByDFS(grid, 0), 1000, 1000);
+	}
+
+	@Test
+	public void test_DFS_10_000_000() {
+		test("Non-Recursive DFS", grid -> createMazeByDFS(grid, 0), 10_000, 1000);
+	}
+}
