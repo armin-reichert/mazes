a545ceebb9301f6ebc3864d8df3f00741b6fa9b5
Armin Reichert <armin.reichert@web.de>
2020-03-04 15:37:32 +0100

    make Kruskal work for 8-neighbor grids

--- mazes-algorithms/src/main/java/de/amr/maze/alg/mst/KruskalMST.java
+++ mazes-algorithms/src/main/java/de/amr/maze/alg/mst/KruskalMST.java
@@ -8,7 +8,6 @@ import static de.amr.graph.grid.impl.GridFactory.fullGrid;
 import de.amr.datastruct.Partition;
 import de.amr.graph.core.api.TraversalState;
 import de.amr.graph.grid.api.GridGraph2D;
-import de.amr.graph.grid.impl.Grid4Topology;
 import de.amr.maze.alg.core.MazeGenerator;
 
 /**
@@ -16,10 +15,11 @@ import de.amr.maze.alg.core.MazeGenerator;
  * 
  * @author Armin Reichert
  * 
- * @see <a href="https://en.wikipedia.org/wiki/Kruskal%27s_algorithm">Kruskal's Algorithm -
- *      Wikipedia</a>
+ * @see <a href="https://en.wikipedia.org/wiki/Kruskal%27s_algorithm">Kruskal's
+ *      Algorithm - Wikipedia</a>
  * 
- * @see <a href="http://weblog.jamisbuck.org/2011/1/3/maze-generation-kruskal-s-algorithm.html">Maze
+ * @see <a href=
+ *      "http://weblog.jamisbuck.org/2011/1/3/maze-generation-kruskal-s-algorithm.html">Maze
  *      Generation: Kruskal's Algorithm</a>
  */
 public class KruskalMST extends MazeGenerator {
@@ -31,7 +31,7 @@ public class KruskalMST extends MazeGenerator {
 	@Override
 	public void createMaze(int x, int y) {
 		Partition<Integer> forest = new Partition<>();
-		GridGraph2D<?, ?> fullGrid = fullGrid(grid.numCols(), grid.numRows(), Grid4Topology.get(), UNVISITED, 0);
+		GridGraph2D<?, ?> fullGrid = fullGrid(grid.numCols(), grid.numRows(), grid.getTopology(), UNVISITED, 0);
 		permute(fullGrid.edges()).forEach(edge -> {
 			int u = edge.either(), v = edge.other();
 			if (forest.find(u) != forest.find(v)) {
