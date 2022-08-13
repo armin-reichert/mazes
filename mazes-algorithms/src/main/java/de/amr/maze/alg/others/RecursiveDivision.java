57c831f0d53bac30790b854af4340be37fccd960
Armin Reichert <armin.reichert@web.de>
2022-08-13 18:35:07 +0200

    refac

--- mazes-algorithms/src/main/java/de/amr/maze/alg/others/RecursiveDivision.java
+++ mazes-algorithms/src/main/java/de/amr/maze/alg/others/RecursiveDivision.java
@@ -12,8 +12,7 @@ import de.amr.maze.alg.core.MazeGenerator;
  * 
  * @author Armin Reichert
  * 
- * @see <a href=
- *      "http://weblog.jamisbuck.org/2011/1/12/maze-generation-recursive-division-algorithm.html">Maze
+ * @see <a href= "http://weblog.jamisbuck.org/2011/1/12/maze-generation-recursive-division-algorithm.html">Maze
  *      Generation: Recursive Division</a>
  */
 public class RecursiveDivision extends MazeGenerator {
@@ -30,35 +29,33 @@ public class RecursiveDivision extends MazeGenerator {
 	}
 
 	/**
-	 * Divides the {@code (w x h)}-subgrid with top-left position {@code (x0, y0)}.
+	 * Divides the {@code (w x h)}-subgrid at top-left position {@code (x0, y0)}.
 	 * 
-	 * @param x0 x-position of subgrid
-	 * @param y0 y-position subgrid
-	 * @param w  width of subgrid
-	 * @param h  height of subgrid
+	 * @param x       top-left x-position
+	 * @param y       top-left y-position
+	 * @param numCols subgrid width
+	 * @param numRows subgrid height
 	 */
-	private void divide(int x0, int y0, int w, int h) {
-		if (w <= 1 && h <= 1) {
+	private void divide(int x, int y, int numCols, int numRows) {
+		if (numCols <= 1 && numRows <= 1) {
 			return;
 		}
-		if (w < h || (w == h && rnd.nextBoolean())) {
-			// Build "horizontal wall" at random y from [y0 + 1, y0 + h - 1], keep random door
-			int y = y0 + 1 + rnd.nextInt(h - 1);
-			int door = x0 + rnd.nextInt(w);
-			range(x0, x0 + w).filter(x -> x != door).forEach(x -> {
-				grid.edge(grid.cell(x, y - 1), grid.cell(x, y)).ifPresent(grid::removeEdge);
-			});
-			divide(x0, y0, w, y - y0);
-			divide(x0, y, w, h - (y - y0));
+		if (numCols < numRows || (numCols == numRows && rnd.nextBoolean())) {
+			// Build "horizontal wall" at random row in range [y + 1, y + numRows - 1), keep random door
+			int row = y + 1 + rnd.nextInt(numRows - 1);
+			int door = x + rnd.nextInt(numCols);
+			range(x, x + numCols).filter(col -> col != door)
+					.forEach(col -> grid.edge(grid.cell(col, row - 1), grid.cell(col, row)).ifPresent(grid::removeEdge));
+			divide(x, y, numCols, row - y);
+			divide(x, row, numCols, numRows - (row - y));
 		} else {
-			// Build "vertical wall" at random x from [x0 + 1, x0 + w - 1], keep random door
-			int x = x0 + 1 + rnd.nextInt(w - 1);
-			int door = y0 + rnd.nextInt(h);
-			range(y0, y0 + h).filter(y -> y != door).forEach(y -> {
-				grid.edge(grid.cell(x - 1, y), grid.cell(x, y)).ifPresent(grid::removeEdge);
-			});
-			divide(x0, y0, x - x0, h);
-			divide(x, y0, w - (x - x0), h);
+			// Build "vertical wall" at random col in range [x + 1, x + numCols - 1), keep random door
+			int col = x + 1 + rnd.nextInt(numCols - 1);
+			int door = y + rnd.nextInt(numRows);
+			range(y, y + numRows).filter(row -> row != door)
+					.forEach(row -> grid.edge(grid.cell(col - 1, row), grid.cell(col, row)).ifPresent(grid::removeEdge));
+			divide(x, y, col - x, numRows);
+			divide(col, y, numCols - (col - x), numRows);
 		}
 	}
 }
