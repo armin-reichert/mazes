58bdd6149d80cbceffb6a7d77a7de7779bc581d9
Armin Reichert <armin.reichert@web.de>
2025-08-30 14:59:20 +0200

    Gradle

---
+++ mazes-simplified/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module mazes.simplified {
+
+    requires de.amr.graph.core;
+    requires de.amr.graph.grid;
+    requires de.amr.graph.pathfinder;
+
+    exports de.amr.mazes.simple;
+    exports de.amr.mazes.simple.graph;
+}
